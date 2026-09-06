8cbfc4ca1489a08de01673ca68fa33805ac173f0
rachmi00 <r.agyingi@gmail.com>
2026-09-06 20:31:03 +0100

    feat: added signals for timer and for a body entering an area

--- scenes/game.gd
+++ scenes/game.gd
@@ -11,3 +11,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 	pass
+
+
+func _on_timer_timeout() -> void:
+	print("Is this for real")
+	
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print(body)
+	print("has entered")
