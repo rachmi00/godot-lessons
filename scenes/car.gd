4e0f12ab927d14bcd9bc6cab790ac73cdf3be1d6
rachmi00 <r.agyingi@gmail.com>
2026-09-10 19:35:53 +0100

    ffeat: destroy car instances as they leave the screen

--- scenes/car.gd
+++ scenes/car.gd
@@ -7,3 +7,7 @@ var speed = 2
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position += direction * speed
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
