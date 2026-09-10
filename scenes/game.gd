dbd697c3e882ffdcb4c13464e4586704839fd09c
rachmi00 <r.agyingi@gmail.com>
2026-09-10 18:45:56 +0100

    created carscene and attatched it to current game scene

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-
+var car_scene: PackedScene = preload("res://scenes/car.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,3 +17,8 @@ func _physics_process(_delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print(body)
 	print("has entered")
+
+
+func _on_car_timer_timeout() -> void:
+	var car = car_scene.instantiate()
+	add_child(car)
